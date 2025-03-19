ff2646bd9a754eb29651c08809e856ebf648456a
Raphael Pereira <raphael.pereira@grupogen.com.br>
2025-03-19 17:01:26 -0300

    :toda: Código inicial

---
+++ example/variables.tf
@@ -0,0 +1,19 @@
+variable "domain_name" {
+  default = "exemplo.com"
+}
+
+variable "subdomain_name" {
+  default = "app.exemplo.com"
+}
+
+variable "lb_dns_name" {
+  default = "meu-lb-123456.elb.amazonaws.com"
+}
+
+variable "lb_zone_id" {
+  default = "Z3AADJGX6KTTL2"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
